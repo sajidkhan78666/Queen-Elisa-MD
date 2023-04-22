0c0ed10393c755d4ef23e48d337b891b41a4151d
sajidkhan78666 <131393212+sajidkhan78666@users.noreply.github.com>
2023-04-22 13:05:34 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -32,15 +32,15 @@ global.APIKeys = {
 }
 
 //other
-global.owner = ['94715166712'] // Owner number , (අයිතිකරුගේ නම්බර් එක )
-global.pemilik = ['94715166712'] //Change  it , ඔබට කැමති නම් වෙනස් කරන්න
-global.premium = ['94715166712'] //Change it ඔබට කැමතිනම් වෙනස් කරන්න 
+global.owner = ['923172362314'] // Owner number , (අයිතිකරුගේ නම්බර් එක )
+global.pemilik = ['923172362314'] //Change  it , ඔබට කැමති නම් වෙනස් කරන්න
+global.premium = ['923172362314'] //Change it ඔබට කැමතිනම් වෙනස් කරන්න 
 global.pengguna = 'Dark Maker' // Your name ඔබේ නම 
 global.botnma = '💃 𝐐𝐮𝐞𝐞𝐧 𝐄𝐥𝐢𝐬𝐚 𝐁𝐨𝐭 💃' // Your bot name , ඔබගෙ බොට්ගේ නම 
 global.ownernma = '𝓓𝓪𝓻𝓴 𝓜𝓪𝓴𝓮𝓻' //ownernama,ownername
 global.packname = 'Queen Elisa Sticker' // Sticker package name 
 global.author = 'Dark Maker' // Sticker Autor name 
-global.sessionName = 'session'
+global.sessionName = 'session'bUp0VzFOeGs=
 global.prefa = ['#','!','/',''] 
 global.sp = '🔵'
 // Alive massage , ඔබේ බොගේ alive massage  එක මෙහි ඇති ` ` මෙම ලකුනු ඇතුලේ ඔබට අවශ්‍ය ඔනිම දෙයක් Type කරන්න.💃
